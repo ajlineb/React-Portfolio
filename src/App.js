8adf9e42014d8b5346e3c3fab2c44297a5a420c3
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-01-01 14:41:18 -0500

    applying css to items, getting bootstrap correct, making site appealing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Tabs from "./components/Tabs";
 import AppContainer from "./components/AppContainer";
 
 function App() {
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,47 +1,63 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 
 export default function Tabs({ currentPage, pageChange }) {
   //These are each of the tabs
   //depending on which tab is clicked sends the pageChange function the page that was clicked
   //also activates that tab
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => pageChange("Home")}
-          className={currentPage === "Home" ? "nav-link active" : "nav-link"}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => pageChange("Works")}
-          className={currentPage === "Works" ? "nav-link active" : "nav-link"}
-        >
-          Works
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => pageChange("Contact")}
-          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => pageChange("Resume")}
-          className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
-        >
-          Resume
-        </a>
-      </li>
-    </ul>
+    <Row className="banner align-items-center">
+      <Col md="6">
+        <h1 className="name display-4">Anthony Linebaugh</h1>
+      </Col>
+      <Col md="6">
+        <ul className="nav nav-pills justify-content-center display-4 ">
+          <li className="nav-item">
+            <a
+              href="#home"
+              onClick={() => pageChange("Home")}
+              className={
+                currentPage === "Home" ? "nav-link active" : "nav-link"
+              }
+            >
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#home"
+              onClick={() => pageChange("Works")}
+              className={
+                currentPage === "Works" ? "nav-link active" : "nav-link"
+              }
+            >
+              Works
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#home"
+              onClick={() => pageChange("Contact")}
+              className={
+                currentPage === "Contact" ? "nav-link active" : "nav-link"
+              }
+            >
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#home"
+              onClick={() => pageChange("Resume")}
+              className={
+                currentPage === "Resume" ? "nav-link active" : "nav-link"
+              }
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </Col>
+    </Row>
   );
 }
