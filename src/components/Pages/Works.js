6c5469a7203c1b3092335f72bf944af0556e9ae9
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-01-02 23:10:11 -0500

    added github links to each of the works using the github logo

--- src/components/Pages/Works.js
+++ src/components/Pages/Works.js
@@ -13,8 +13,17 @@ export default function Works() {
               src={require("../images/AstroProject.png")}
               alt="Space website"
             ></img>
-            <p className="text-block">Astronomical Guidance</p>
           </a>
+          <p className="text-block">
+            Astronomical Guidance<br></br>
+            <a href="https://github.com/jparris3213/UNC_Project_1_Group">
+              <img
+                id="gitLink"
+                src={require("../images/GitHub-Mark-Light-64px.png")}
+                alt="github"
+              />
+            </a>
+          </p>
         </Col>
         <Col sm="6" className="image-container">
           <a href="https://philip-project2.herokuapp.com/">
@@ -23,8 +32,17 @@ export default function Works() {
               src={require("../images/ForumProject.png")}
               alt="Forum webpage"
             ></img>
-            <p className="text-block">Riposte Forum</p>
           </a>
+          <p className="text-block">
+            Riposte Forum<br></br>
+            <a href="https://github.com/Terrence-Me/project2-team-one">
+              <img
+                id="gitLink"
+                src={require("../images/GitHub-Mark-Light-64px.png")}
+                alt="github"
+              />
+            </a>
+          </p>
         </Col>
       </Row>
       <Row className="align-items-center image-rows">
@@ -35,8 +53,17 @@ export default function Works() {
               src="https://raw.githubusercontent.com/ajlineb/Object-Relational-Mapping-E-Commerce-Backend/main/images/E_comm.PNG"
               alt="Forum webpage"
             ></img>
-            <p className="text-block">E-Commerce Backend</p>
           </a>
+          <p className="text-block">
+            E-Commerce Backend<br></br>
+            <a href="https://github.com/ajlineb/Object-Relational-Mapping-E-Commerce-Backend">
+              <img
+                id="gitLink"
+                src={require("../images/GitHub-Mark-Light-64px.png")}
+                alt="github"
+              />
+            </a>
+          </p>
         </Col>
         <Col sm="6" className="image-container">
           <a href="https://budget-tracker-0-1.herokuapp.com/">
@@ -45,8 +72,17 @@ export default function Works() {
               src="https://raw.githubusercontent.com/ajlineb/Proggressive-Budget-Tracker/main/images/BudgetTracker.png"
               alt="Forum webpage"
             ></img>
-            <p className="text-block">Budget Tracker Progressive Web App</p>
           </a>
+          <p className="text-block">
+            Budget Tracker Progressive Web App<br></br>
+            <a href="https://github.com/ajlineb/Proggressive-Budget-Tracker">
+              <img
+                id="gitLink"
+                src={require("../images/GitHub-Mark-Light-64px.png")}
+                alt="github"
+              />
+            </a>
+          </p>
         </Col>
       </Row>
       <Row className="align-items-center image-rows">
@@ -57,8 +93,18 @@ export default function Works() {
               src="https://raw.githubusercontent.com/ajlineb/SQL-Employee-Tracker/main/images/employeeDatabase.png"
               alt="Forum webpage"
             ></img>
-            <p className="text-block">SQL Employee Tracker</p>
           </a>
+          <p className="text-block">
+            SQL Employee Tracker
+            <br></br>
+            <a href="https://github.com/ajlineb/SQL-Employee-Tracker">
+              <img
+                id="gitLink"
+                src={require("../images/GitHub-Mark-Light-64px.png")}
+                alt="github"
+              />
+            </a>
+          </p>
         </Col>
         <Col sm="6" className="image-container">
           <a href="https://ajlineb.github.io/Weather_Dashboard/">
@@ -67,8 +113,17 @@ export default function Works() {
               src="https://raw.githubusercontent.com/ajlineb/Weather_Dashboard/main/Assets/Images/Weather_Dashboard.png"
               alt="Forum webpage"
             ></img>
-            <p className="text-block">Weather Dashboard APIs</p>
           </a>
+          <p className="text-block">
+            Weather Dashboard APIs<br></br>
+            <a href="https://github.com/ajlineb/Weather_Dashboard">
+              <img
+                id="gitLink"
+                src={require("../images/GitHub-Mark-Light-64px.png")}
+                alt="github"
+              />
+            </a>
+          </p>
         </Col>
       </Row>
     </div>
