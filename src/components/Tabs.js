0eba40ca713f3806ad04c83d70029b956a10cff2
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-02-04 19:40:02 -0500

    updated the active tabs so that upon arriving the home tab is active

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -2,9 +2,23 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Tabs() {
-  const tabs = ["/home", "/works", "/contact", "/resume"];
+  const tabs = ["/React-Portfolio", "/works", "/contact", "/resume"];
   const [active, setActive] = useState(tabs[0]);
   console.log(tabs[0]);
+
+  function handleTab(tab) {
+    if (tab === tabs[0]) {
+      return "Home";
+    }
+    if (tab === tabs[1]) {
+      return "Works";
+    }
+    if (tab === tabs[2]) {
+      return "Contact";
+    } else {
+      return "Resume";
+    }
+  }
 
   return (
     <nav>
@@ -20,8 +34,9 @@ export default function Tabs() {
                 window.location.pathname === tab ? "tabs tab-active" : "tabs"
               }
             >
-              {tab.substring(1).charAt(0).toUpperCase() +
-                tab.slice(2, tab.length)}
+              {/* {tab.substring(1).charAt(0).toUpperCase() +
+                tab.slice(2, tab.length)} */}
+              {handleTab(tab)}
             </Link>
           </li>
         ))}
