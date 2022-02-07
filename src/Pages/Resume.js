3964f1672cdbf2930b20a9e98a8d42cdf131ca5a
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-02-07 00:20:52 -0500

    updated how the resume appears if the user supports a PDF plugin, updated how the known languages images size when on smaller screens

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -24,29 +24,33 @@ export default function Resume() {
       <h1 className="title">Resume</h1>
       <Row className="align-items-center ">
         <Col md="6" className="pdf-container">
-          <embed
-            src={require("../pdf/AnthonyLinebaughResume2022.pdf")}
+          <object
+            data={require("../pdf/AnthonyLinebaughResume2022.pdf")}
             width={"1000px"}
             height={"1000px"}
-          ></embed>
-          <h1>
-            If the PDF is not showing, here is a link to my Resume!
-            <br></br>
-            <a href="https://drive.google.com/file/d/14CHrwzS_XS9ysWjcta1b9dHoajlVi69e/view?usp=sharing">
-              <img
-                id="resume"
-                src={require("../images/paper_image.png")}
-                alt="document"
-              ></img>
-            </a>
-          </h1>
+          >
+            Your web browser does not support a PDF plugin. Instead you can try
+            <h1>
+              If the PDF is not showing, here is a link to my Resume!
+              <br></br>
+              <a href="https://drive.google.com/file/d/14CHrwzS_XS9ysWjcta1b9dHoajlVi69e/view?usp=sharing">
+                <img
+                  id="resume"
+                  src={require("../images/paper_image.png")}
+                  alt="document"
+                ></img>
+              </a>
+            </h1>
+          </object>
         </Col>
         <Col md="6" className="resume-container">
           <div className="resume-info">
             <h1>Languages and Experience</h1>
             <ul>
               {experiences.map((experience) => (
-                <li key={experience.id}>{experience}</li>
+                <li key={experience.id} className="exp-nolist">
+                  {experience}
+                </li>
               ))}
             </ul>
           </div>
