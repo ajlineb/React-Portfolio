c4aa55b9ceb8688d19521baddb2204988fa4ac38
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-02-10 22:54:50 -0500

    added loading image if images in works are still loadin in

--- src/components/workItem.js
+++ src/components/workItem.js
@@ -1,23 +1,34 @@
 import React from "react";
 import data from "../utils/workList";
 import { Col } from "react-bootstrap";
+import Loading from "../utils/Loading";
 
 export default function WorkItem() {
-  console.log(data);
-  return data.map((d) => (
-    <Col key={d.id} sm="6" className="image-container">
-      <a id="linkContainer" href={d.deployLink}>
-        <img id="showcase" src={d.img.src} alt={d.img.alt}></img>
-        <p className="text-block">
-          {d.projectName}
-          <br></br>
-          <a href={d.gitLink}>
-            <img id="gitLink" src={d.gitImg.src} alt={d.gitImg.alt} />
-          </a>
-        </p>
-      </a>
+  console.log(data.length);
+  return (
+    <>
+      {data.length ? (
+        data.map((d) => {
+          return (
+            <Col key={d.id} sm="6" className="image-container">
+              <a id="linkContainer" href={d.deployLink}>
+                <img id="showcase" src={d.img.src} alt={d.img.alt}></img>
+                <p className="text-block">
+                  {d.projectName}
+                  <br></br>
+                  <a href={d.gitLink}>
+                    <img id="gitLink" src={d.gitImg.src} alt={d.gitImg.alt} />
+                  </a>
+                </p>
+              </a>
 
-      <p id="workInfo">{d.info}</p>
-    </Col>
-  ));
+              <p id="workInfo">{d.info}</p>
+            </Col>
+          );
+        })
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
 }
---
+++ src/utils/Loading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <>
+      <p>Loading...</p>
+      <img
+        className="loading"
+        src="https://c.tenor.com/hBV2DeZaNiUAAAAC/loading-icon.gif"
+        alt="..."
+      ></img>
+    </>
+  );
+}
+
+export default Loading;
